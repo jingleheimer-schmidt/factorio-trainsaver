d0e0161cca15157b9a866c97869925d38e2b6226
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-11-11 10:53:20 -0800

    Update control.lua

--- control.lua
+++ control.lua
@@ -1,5 +1,5 @@
--- [[
+-- {{
 on command get all trains. find if train is moving, then make cutscene for various moving trains. 
 still play cutscene if no trains are moving? i think yes, but cut to moving train as soon as one is available
 
-]]
+}}
