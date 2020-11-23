21abb94b710a3c4b00cda12bffc61c607f72bffc
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-11-22 23:41:39 -0800

    removed comments

--- control.lua
+++ control.lua
@@ -18,7 +18,6 @@ function start_trainsaver(command)
       game.print("no trains available :(")
       return
     else
-      -- game.print("syncing color and begining cutscene")
       local table_of_active_trains = {}
       for a,b in pairs(table_of_trains) do
         if b.state == defines.train_state.on_the_path then
@@ -28,22 +27,18 @@ function start_trainsaver(command)
       if table_of_active_trains[1] then
         local waypoint_target = {}
         local random_train_index = math.random(table_size(table_of_active_trains))
-        -- game.print("found active train with state " .. table_of_active_trains[random_train_index].state)
         if table_of_active_trains[random_train_index].locomotives.front_movers[1] then
-          -- game.print("found front mover [1]")
           local waypoint_target = table_of_active_trains[random_train_index].locomotives.front_movers[1]
           local created_waypoints = create_starting_waypoint(waypoint_target, player_index)
           sync_color(player_index)
           play_cutscene(created_waypoints, player_index)
         else
-          -- game.print("found back mover [1]")
           local waypoint_target = table_of_active_trains[random_train_index].locomotives.back_movers[1]
           local created_waypoints = create_starting_waypoint(waypoint_target, player_index)
           sync_color(player_index)
           play_cutscene(created_waypoints, player_index)
         end
       else
-        -- game.print("could not find active train")
         if table_of_trains[1].locomotives.front_movers[1] then
           local waypoint_target = table_of_trains[1].locomotives.front_movers[1]
           local created_waypoints = create_starting_waypoint(waypoint_target, player_index)
@@ -67,9 +62,9 @@ function create_starting_waypoint(waypoint_target, player_index)
   if mod_settings["ts-transition-time"].value == 0 then
     tt = 1
   else
-    tt = mod_settings["ts-transition-time"].value * 60 -- convert seconds to ticks
+    tt = mod_settings["ts-transition-time"].value * 60
   end
-  local tw = mod_settings["ts-time-wait"].value * 60 * 60 -- convert minutes to ticks
+  local tw = mod_settings["ts-time-wait"].value * 60 * 60
   if mod_settings["ts-variable-zoom"].value == true then
     local temp_zoom = mod_settings["ts-zoom"].value
     z = (math.random(((temp_zoom - (temp_zoom*.2))*1000),(((temp_zoom + (temp_zoom*.2)))*1000)))/1000
@@ -131,15 +126,12 @@ script.on_event(defines.events.on_train_changed_state, function(train_changed_st
             if found_train.id == train.id then
               create_cutscene_next_tick = {}
               create_cutscene_next_tick[player_index] = {train, player_index}
-              -- game.print("global: " .. serpent.line(create_cutscene_next_tick[player_index]))
             end
           else
             create_cutscene_next_tick = {}
             create_cutscene_next_tick[player_index] = {train, player_index}
-            -- game.print("global: " .. serpent.line(create_cutscene_next_tick[player_index]))
           end
         else
-          -- game.print("no locomotive near player")
         end
       end
     end
@@ -153,37 +145,25 @@ script.on_event(defines.events.on_tick, function()
       local player_index = b[2]
       if ((target_train.locomotives.front_movers[1]) and (target_train.locomotives.back_movers[1])) then
         if target_train.speed > 0 then
-          -- game.print("front and back available: starting new cutscene on front mover")
-          -- game.print("global: " .. serpent.line(create_cutscene_next_tick[player_index]))
           local created_waypoints = create_lead_locomotive_waypoint(target_train.locomotives.front_movers[1], player_index)
           play_cutscene(created_waypoints, player_index)
           create_cutscene_next_tick[player_index] = nil
-          -- game.print("global: " .. serpent.line(create_cutscene_next_tick[player_index]))
         end
         if target_train.speed < 0 then
-          -- game.print("front and back available: starting new cutscene on back mover")
-          -- game.print("global: " .. serpent.line(create_cutscene_next_tick[player_index]))
           local created_waypoints = create_lead_locomotive_waypoint(target_train.locomotives.back_movers[1], player_index)
           play_cutscene(created_waypoints, player_index)
           create_cutscene_next_tick[player_index] = nil
-          -- game.print("global: " .. serpent.line(create_cutscene_next_tick[player_index]))
         end
       elseif ((target_train.locomotives.front_movers[1]) or (target_train.locomotives.back_movers[1])) then
         if target_train.locomotives.front_movers[1] then
-          -- game.print("only front available: starting new cutscene on front mover")
-          -- game.print("global: " .. serpent.line(create_cutscene_next_tick[player_index]))
           local created_waypoints = create_lead_locomotive_waypoint(target_train.locomotives.front_movers[1], player_index)
           play_cutscene(created_waypoints, player_index)
           create_cutscene_next_tick[player_index] = nil
-          -- game.print("global: " .. serpent.line(create_cutscene_next_tick[player_index]))
         end
         if target_train.locomotives.back_movers[1] then
-          -- game.print("only front available: starting new cutscene on front mover")
-          -- game.print("global: " .. serpent.line(create_cutscene_next_tick[player_index]))
           local created_waypoints = create_lead_locomotive_waypoint(target_train.locomotives.front_movers[1], player_index)
           play_cutscene(created_waypoints, player_index)
           create_cutscene_next_tick[player_index] = nil
-          -- game.print("global: " .. serpent.line(create_cutscene_next_tick[player_index]))
         end
       end
     end
@@ -197,9 +177,9 @@ function create_lead_locomotive_waypoint(locomotive, player_index)
   if mod_settings["ts-transition-time"].value == 0 then
     tt = 1
   else
-    tt = mod_settings["ts-transition-time"].value * 60 -- convert seconds to ticks
+    tt = mod_settings["ts-transition-time"].value * 60
   end
-  local tw = mod_settings["ts-time-wait"].value * 60 * 60 -- convert minutes to ticks
+  local tw = mod_settings["ts-time-wait"].value * 60 * 60
   if mod_settings["ts-variable-zoom"].value == true then
     local temp_zoom = mod_settings["ts-zoom"].value
     z = (math.random(((temp_zoom - (temp_zoom*.2))*1000),(((temp_zoom + (temp_zoom*.2)))*1000)))/1000
