a18cf7b8aa8ed353cfafd360f4b1d951499f1050
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2021-01-14 23:42:42 -0800

    added more hotkeys for linked game controls to exit trainsaver

--- data.lua
+++ data.lua
@@ -1,20 +1,3 @@
-local escapeTrainsaverKey = {
-  type = "custom-input",
-  name = "escape-trainsaver",
-  key_sequence = "ESCAPE",
-  alternative_key_sequence = "RETURN",
-  enabled_while_in_cutscene = true,
-  action = "lua",
-}
-
-local openGuiTrainsaverKey = {
-  type = "custom-input",
-  name = "open-inventory-trainsaver",
-  linked_game_control = "open-gui"
-  enabled_while_in_cutscene = true,
-  action = "lua",
-}
-
 local toggleTrainsaverKey = {
   type = "custom-input",
   name = "toggle-trainsaver",
@@ -24,8 +7,76 @@ local toggleTrainsaverKey = {
   action = "lua",
 }
 
+local startTrainsaverKey = {
+  type = "custom-input",
+  name = "start-trainsaver",
+  key_sequence = "",
+  alternative_key_sequence = "",
+  enabled_while_in_cutscene = true,
+  action = "lua",
+}
+
+local endTrainsaverKey = {
+  type = "custom-input",
+  name = "end-trainsaver",
+  key_sequence = "",
+  alternative_key_sequence = "",
+  enabled_while_in_cutscene = true,
+  action = "lua",
+}
+
+local openInventoryTrainsaverKey = {
+  type = "custom-input",
+  name = "open-inventory-trainsaver",
+  key_sequence = "",
+  linked_game_control = "open-character-gui",
+  enabled_while_in_cutscene = true,
+  action = "lua",
+}
+
+local openTechnologyGuiTrainsaverKey = {
+  type = "custom-input",
+  name = "open-research-trainsaver",
+  key_sequence = "",
+  linked_game_control = "open-technology-gui",
+  enabled_while_in_cutscene = true,
+  action = "lua",
+}
+
+local openProductionStatsGuiTrainsaverKey = {
+  type = "custom-input",
+  name = "open-production-stats-trainsaver",
+  key_sequence = "",
+  linked_game_control = "production-statistics",
+  enabled_while_in_cutscene = true,
+  action = "lua",
+}
+
+local openLogisticNetworkGuiTrainsaverKey = {
+  type = "custom-input",
+  name = "open-logistic-netowrk-trainsaver",
+  key_sequence = "",
+  linked_game_control = "logistic-networks",
+  enabled_while_in_cutscene = true,
+  action = "lua",
+}
+
+local toggleMenuTrainsaverKey = {
+  type = "custom-input",
+  name = "toggle-menu-trainsaver",
+  key_sequence = "",
+  linked_game_control = "toggle-menu",
+  enabled_while_in_cutscene = true,
+  action = "lua",
+}
+
 data:extend({
-  escapeTrainsaverKey,
-  openGuiTrainsaverKey,
   toggleTrainsaverKey,
+  startTrainsaverKey,
+  endTrainsaverKey,
+  openInventoryTrainsaverKey,
+  openTechnologyGuiTrainsaverKey,
+  openProductionStatsGuiTrainsaverKey,
+  openLogisticNetworkGuiTrainsaverKey,
+  toggleMenuTrainsaverKey,
 })
