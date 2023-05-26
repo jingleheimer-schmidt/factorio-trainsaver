728b1b95fb36d6170610a83342ee6a0cd28fbcfa
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-05-26 15:49:45 -0700

    add locomotive name to chatty messages

--- control.lua
+++ control.lua
@@ -98,7 +98,7 @@ end
 local function chatty_target_entity_name(entity)
   local id = entity.backer_name or entity.unit_number or script.register_on_entity_destroyed(entity)
   local target_name = entity.name .. " " .. id
-  if entity.train then target_name = "train " .. entity.train.id end
+  if entity.train then target_name = "train " .. entity.train.id .. ", " .. target_name end
   local color = entity.color
   if color then
     target_name = "[color=" .. color.r .. "," .. color.g .. "," .. color.b .. "]" .. target_name .. "[/color]"
